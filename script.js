4605e61184aa3ad082c25e714552fafdf3e3fb35
luNoBrasil <149010489+luNoBrasil@users.noreply.github.com>
2023-10-26 10:11:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -82,6 +82,7 @@ function updateCountdown() {
         const imageSrc = `imagens/imagem${messageIndex}.jpg`;
         const imageElement = document.createElement("img");
         imageElement.src = imageSrc;
+        imageElement.style.maxWidth = "100%"; // A imagem terá no máximo a largura da tela do celular
         imageContainer.innerHTML = '';
         imageContainer.appendChild(imageElement);
     }
