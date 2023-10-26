80b40d025147e6fe784b9918d373a578532eacd0
luNoBrasil <149010489+luNoBrasil@users.noreply.github.com>
2023-10-26 08:50:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var targetDate = new Date('2023-12-16T00:10:00');
+// Data alvo da contagem regressiva (16/12/2023)
+const targetDate = new Date('2023-12-16T07:10:00');
 
 var phrases = [
     "52 Linha de teste 52",
@@ -58,28 +59,27 @@ var phrases = [
 
 // Função para calcular o tempo restante e atualizar a exibição
 function updateCountdown() {
-    var currentDate = new Date();
-    var timeDifference = targetDate - currentDate;
+    const currentDate = new Date();
+    const timeDifference = targetDate - currentDate;
 
     if (timeDifference > 0) {
-        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 
-        var countdownText = `Faltam ${days} dias, ${hours} horas e ${minutes} minutos para 16/12/2023 07:10`;
+        const countdownText = `Faltam ${days} dias, ${hours} horas e ${minutes} minutos para 16/12/2023.`;
         document.getElementById('countdown-text').textContent = countdownText;
-        
-        // Exibir a frase do dia
-        var dailyPhrase = document.getElementById('daily-phrase');
-        dailyPhrase.textContent = phrases[currentDay - 1]; // Subtrai 1 para corresponder ao índice do array
-        
+
         // Trocar a imagem a cada dia
-        var currentDay = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
-        var imageContainer = document.getElementById('image-container');
-        var dailyImage = document.getElementById('daily-image');
+        const currentDay = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + 1;
+        const imageContainer = document.getElementById('image-container');
+        const dailyImage = document.getElementById('daily-image');
         dailyImage.src = `imagens/imagem${currentDay}.jpg`; // Substitua pelo caminho correto das suas imagens
         dailyImage.alt = `Imagem do Dia ${currentDay}`;
-  
+
+        // Exibir a frase do dia
+        const dailyPhrase = document.getElementById('daily-phrase');
+        dailyPhrase.textContent = phrases[currentDay - 1]; // Subtrai 1 para corresponder ao índice do array
     } else {
         document.getElementById('countdown-text').textContent = "Chegou o grande dia!";
     }
